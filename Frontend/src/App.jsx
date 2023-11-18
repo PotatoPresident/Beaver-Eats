bc8c887761bd2d60842cfebe339165863c6bca61
Chris H <officialchrisho@gmail.com>
2023-11-17 19:10:21 -0800

    commit without sensitive info

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,18 +4,18 @@ import Navbar from './components/navbar'
 export default function App() {
 
   return (
-    <div className='flex'> 
-      <Navbar/>
+    <div className='flex'>
+      <Navbar />
       <div id='map' className='flex w-full' />
     </div>
   )
 }
 
 const loader = new Loader({
-  apiKey: "AIzaSyCs4CKMrfMTvNVsxSHtf0LB6F2RTUjBW7M", //import.meta.env.VITE_GOOGLE_API_KEY,
+  apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
   version: 'weekly'
 })
-loader.load().then(async() => {
+loader.load().then(async () => {
   const { Map } = await google.maps.importLibrary('maps')
 
   new Map(document.getElementById('map'), {
