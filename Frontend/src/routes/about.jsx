f41a1320a0659da1942e5615229703c5c93ae033
Chris H <officialchrisho@gmail.com>
2023-11-19 13:44:30 -0800

    Updated about page, linked attribution beaver icon

--- Frontend/src/routes/about.jsx
+++ Frontend/src/routes/about.jsx
@@ -4,10 +4,12 @@ export default function About() {
     <div className='flex justify-center items-center w-screen h-screen bg-model'>
       <main className='bg-neutral-800 w-[700px] h-[500px] rounded-lg border border-neutral-600 shadow-default'>
         <div className='flex flex-col py-8 px-8 text-neutral-50'>
-          <p>Lead Backend Developer: Will https://github.com/PotatoPresident</p>
-          <p>owen</p>
-          <p>chris</p>
-          <p>sawyer</p>
+          <p>Lead Backend Developer: Will <br /> <a href="https://github.com/PotatoPresident">https://github.com/PotatoPresident</a></p><br /> 
+          <p>Lead Frontend Developer: Owen <br /> <a href="https://github.com/owenkrause">https://github.com/owenkrause</a></p><br /> 
+          <p>Frontend Developer: Chris <br /> <a href="https://github.com/chostudio">https://github.com/chostudio</a></p><br /> 
+          <p>Frontend Developer: Sawyer <br /> <a href="https://github.com/SawyerJLamont">https://github.com/SawyerJLamont</a></p><br /> 
+          <br />
+          <p>Beaver Icon Attribution: <a href="https://thenounproject.com/icon/beaver-1604196/">https://thenounproject.com/icon/beaver-1604196/</a></p>
         </div>
       </main>
     </div>
