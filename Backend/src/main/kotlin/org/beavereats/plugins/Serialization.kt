aebe9290206788966f7227001c00620d478825a1
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2023-11-18 23:08:31 -0800

    Add more backend data

--- Backend/src/main/kotlin/org/beavereats/plugins/Serialization.kt
+++ Backend/src/main/kotlin/org/beavereats/plugins/Serialization.kt
@@ -3,14 +3,13 @@ package org.beavereats.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json(Json {
             ignoreUnknownKeys = true
+            encodeDefaults = true
         })
     }
 }
