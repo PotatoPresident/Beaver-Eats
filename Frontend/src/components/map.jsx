27ca7fde869e004393ac6bee195875eaeb9ca2e7
owenkrause <owenjkrause@gmail.com>
2023-11-19 14:57:44 -0800

    map switch case

--- Frontend/src/components/map.jsx
+++ Frontend/src/components/map.jsx
@@ -1,7 +1,7 @@
 import { Loader } from '@googlemaps/js-api-loader'
 
 export default function Map({ state }) {
-  const [selectedLocation, setSelectedLocation] = state;
+  const [selectedLocation, setSelectedLocation] = state
   const loader = new Loader({
     apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
     version: 'weekly'
@@ -23,8 +23,10 @@ export default function Map({ state }) {
         'content-type': 'application/json'
       }
     })).json()
+
     const { Map } = await google.maps.importLibrary('maps')
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary('marker')
+    
     const map = new Map(document.getElementById('map'), {
       mapId: '54153603699e2b81',
       center: CORVALLIS,
@@ -43,35 +45,35 @@ export default function Map({ state }) {
           map,
           position: {lat: option.lat, lng: option.lng},
           title: option.displayName,
-        });
-        marker.addListener("click", () => {
+        })
+        marker.addListener('click', () => {
           setSelectedLocation(option.group)
           const clickedMarkerCoords = {lat: option.lat, lng: option.lng}
           map.panTo(clickedMarkerCoords)
           console.log('map paned to ' + clickedMarkerCoords)
-       });
-if (selectedLocation === 'McNary') {
-  map.panTo({lat:44.56412339072111 , lng:-123.27237759512379 })
-  map.setZoom(18)
-}
-else if(selectedLocation === 'Arnold') {
-  map.panTo({lat:44.56045267241557 , lng:-123.27759333004018 })
-  map.setZoom(18)
-}
-else if(selectedLocation === 'West') {
-  map.panTo({lat:44.56390497552497,  lng:-123.2835802319097})
-  map.setZoom(18)
-}
-
-else if(selectedLocation === 'MU') {
-  map.panTo({lat:44.5649206643527,  lng: -123.2789069030742 })
-  map.setZoom(18)
-}
-
-else if(selectedLocation === 'Other') {
-  map.panTo({lat:44.5649206643527,  lng: -123.2789069030742 })
-  map.setZoom(16.75)
-} 
+        })
+        switch (selectedLocation) {
+          case 'McNary':
+            map.panTo({ lat: 44.56412339072111, lng: -123.27237759512379 })
+            map.setZoom(18)
+            break
+          case 'Arnold':
+            map.panTo({ lat: 44.56045267241557, lng: -123.27759333004018 })
+            map.setZoom(18)
+            break
+          case 'West':
+            map.panTo({ lat: 44.56390497552497, lng: -123.2835802319097 })
+            map.setZoom(18)
+            break
+          case 'MU':
+            map.panTo({ lat: 44.5649206643527, lng: -123.2789069030742 })
+            map.setZoom(18)
+            break
+          case 'Other':
+            map.panTo({ lat: 44.5649206643527, lng: -123.2789069030742 })
+            map.setZoom(16.75)
+            break
+        }
       })
     })
   })
