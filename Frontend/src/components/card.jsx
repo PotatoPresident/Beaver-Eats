4fe0cecb299135dd46436d12f71892c92ae0cb90
owenkrause <owenjkrause@gmail.com>
2023-11-19 16:36:54 -0800

    styling

--- Frontend/src/components/card.jsx
+++ Frontend/src/components/card.jsx
@@ -22,22 +22,22 @@ export default function Cards({ option }) {
 
   const [isHoursVisible, setIsHoursVisible] = useState(false)
   const [isReviewsVisible, setIsReviewsVisible] = useState(false)
-  const [isImagesVisible, setIsImagesVisible] = useState(false)
+  const [isOverviewVisible, setOverview] = useState(false)
 
   const handleClick = section => {
 
-    if (section === 'hours') {
+    if (section === 'overview') {
+      setOverview(!isOverviewVisible)
+      setIsHoursVisible(false)
+      setIsReviewsVisible(false)
+    } else if (section === 'hours') {
       setIsHoursVisible(!isHoursVisible)
       setIsReviewsVisible(false)
-      setIsImagesVisible(false)
+      setOverview(false)
     } else if (section === 'reviews') {
       setIsReviewsVisible(!isReviewsVisible)
       setIsHoursVisible(false)
-      setIsImagesVisible(false)
-    } else if (section === 'images') {
-      setIsImagesVisible(!isImagesVisible)
-      setIsHoursVisible(false)
-      setIsReviewsVisible(false)
+      setOverview(false)
     }
   }
 
@@ -54,23 +54,27 @@ export default function Cards({ option }) {
 
   return (
     <div className='flex flex-col justify-center items-center w-[400px] min-w-[400px] h-[300px] bg-neutral-100 border border-neutral-400 rounded-md shadow-xl'>
-      <img className='w-auto h-24' src={option.images[0]}></img>
-      <span className='font-GeistBold'>{option.displayName}</span>
-
-      <div className='h-20 mt-3 mb-1 overflow-y-auto mx-2 px-5'>
-        <span className='overflow-y-clip text-sm text-neutral-500'>{option.description}</span>
-      </div>
+      <img className='rounded w-auto h-24' src={option.images[0]}></img>
+      <span className='font-GeistBold pt-2'>{option.displayName}</span>
       
-      <div className='cursor-pointer flex text-sm text-neutral-500 gap-4 py-2'>
-        <div className="relative items-baseline hover:outline w-16 h-5 rounded">
+      <div className='cursor-pointer flex text-sm text-neutral-500 gap-4 pt-2'>
+        <div className='relative items-baseline hover:text-neutral-900 w-16 h-5 rounded'>
+          <span className='flex justify-center items-center' onClick={() => handleClick('overview')}>Overview</span>
+        </div>
+        <div className='relative items-baseline hover:text-neutral-900 w-16 h-5 rounded'>
           <span className='flex justify-center items-center' onClick={() => handleClick('hours')}>Hours</span>
         </div>
-        <div className="relative items-baseline hover:outline w-16 h-5 rounded"><span className='flex justify-center items-center' onClick={() => handleClick('reviews')}>Reviews</span>
-        </div>
-        <div className="relative items-baseline hover:outline w-16 h-5 rounded"><span className='flex justify-center items-center' onClick={() => handleClick('images')}>Images</span>
+        <div className='relative items-baseline hover:text-neutral-900 w-16 h-5 rounded'>
+          <span className='flex justify-center items-center' onClick={() => handleClick('reviews')}>Reviews</span>
         </div>
       </div>
+      <hr className='w-2/3 border-neutral-400 pb-2'></hr>
 
+      { isOverviewVisible && (
+        <div className='h-20 overflow-y-auto px-4'>
+          <span className='overflow-y-clip text-sm text-neutral-500'>{option.description}</span>
+        </div>
+      )}
       { isHoursVisible && (
         <div className='flex flex-col text-sm text-neutral-500'>
           <span className='text-sm text-neutral-500'>Hours: {formatHours(option.opens)}-{formatHours(option.closes)}</span>
@@ -78,29 +82,33 @@ export default function Cards({ option }) {
       )}
       { isReviewsVisible && (
         <div className='flex flex-col text-sm text-neutral-500'>
-          <span className='text-sm text-neutral-500'>Rating: {option.rating.toFixed(1)}</span>
-          { reviews.map(review => {
-            return (
+          <span className='text-sm text-neutral-500'>{option.rating.toFixed(1)} Stars</span>
+          <div className='py-2'>
+          <div className='relative max-h-[50px] flex flex-col overflow-y-auto'>
+            { reviews.map(review => {
+              return (
                 <span className='text-sm text-neutral-500'>{review.review}</span>
-            )
-          })}
+              )
+            })}
+          </div>
+          </div>
           <form method='post' onSubmit={handleSubmit}>
             <input
-            name='rating'
-            placeholder='Rating'
-            type='number'
-            min={1}
-            max={5}
-            step={0.1}
-            required
-            style={{ minWidth: '60px', appearance:'none', margin: '0 5px'
-            }}/>
-            
-
+              className='rounded min-w-[50px] border border-neutral-400 p-1 outline-none'
+              name='rating'
+              placeholder='Rating'
+              type='number'
+              min={1}
+              max={5}
+              step={0.1}
+              required
+            />
             <input
-            name='comment'
-            placeholder='Comment'
-            style={{margin:'0 15px'}}/>
+              className='rounded min-w-[80px] border border-neutral-400 p-1 outline-none'
+              name='comment'
+              placeholder='Comment'
+              style={{margin:'0 15px'}}
+            />
             
             <button
             type='submit'>Submit Review
@@ -109,10 +117,6 @@ export default function Cards({ option }) {
           </form>
         </div>
       )}
-      {/* { isImagesVisible && (
-        
-      )} */}
-
     </div>
   )
 }
